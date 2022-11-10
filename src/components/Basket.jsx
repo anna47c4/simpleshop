a38e4806f221fec0803b8c77ec15e0b869d8636d
Anna Maria Lester <anna47c4@stud.kea.dk>
2022-11-10 11:50:50 +0100

    basic set up & components

---
+++ src/components/Basket.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Basket() {
+  return (
+    <section className="Basket">
+      <ul>
+        <li>someProduct x 3, 123,-</li>
+      </ul>
+      <button>Buy now</button>
+    </section>
+  );
+}
+
+export default Basket;
---
+++ src/components/ProductList.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Product from "./Product";
+
+function ProductList(props) {
+  return (
+    <main className="ProductList">
+      {props.products.map((product) => (
+        <Product key={product.id} {...product} />
+      ))}
+    </main>
+  );
+}
+
+export default ProductList;
